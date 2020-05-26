f082f5932144686eb776efce0dbfcc8d3b5e97aa
Nicolas Brondin-Bernard <nicolas@brondin.com>
2020-05-26 21:43:57 +0200

    [FEATURE] Added minimized button

--- src/CookieConsent.js
+++ src/CookieConsent.js
@@ -1,40 +1,49 @@
 import template from './template.html'
 import css from './template.css'
+import cookie_svg from './assets/cookie-bite-solid.svg'
 
 var CookieConsent = function (){
+    this.expanded = true;
     this.consent = null;
+
     this.show_more = function(){
-        cookie_popup.style.display = "initial";
-        
+        cookie_popup.style.display = "block";   
+    }
+    this.close = function(){
+        document.getElementById('cookie-expand').style.display = "block";
+        document.getElementById('cookie-band-large').style.display = "none";
+    }
+
+    this.expand = function(){
+        document.getElementById('cookie-band-large').style.display = "block";
+        document.getElementById('cookie-expand').style.display = "none";
     }
 
     var style = document.createElement('style');
     style.innerText= css;
     document.head.prepend(style);
-    //var style_sheet = style.sheet;
-/*
-    style_sheet.insertRule('.cookie-consent--card {font-family: Arial, sans-serif; font-size: 14px; background: white; padding: 10px; box-sizing: border-box; border-radius: 10px; box-shadow: 0px 0px 10px 5px rgba(0,0,0,0.1);color: rgba(0,0,0,0.9);}',0);
-    style_sheet.insertRule('.cookie-consent--button {background: #3498db;color: white; border: none;border-radius: 20px;margin: 0px 5px; padding: 2px 10px; font-weight: bold;}',0);
-    style_sheet.insertRule('#cookie-band {position: fixed;z-index: 99999;bottom: 10px;right: 10px;left: 10px;}',0);
-    style_sheet.insertRule('#cookie-popup {position: absolute;z-index: 99999;bottom: calc(100% + 10px);left: 0px; max-width: 300px;}',0);
-*/
+    
     var cookie_bar_element = document.createElement("div");
-    cookie_bar_element.id = "cookie-band";
-    cookie_bar_element.classList = ["cookie-consent--card"];
-    cookie_bar_element.innerHTML = template;
+    cookie_bar_element.innerHTML = template.replace('{{cookie}}', cookie_svg);
     
     document.body.appendChild(cookie_bar_element);
 
     var cookie_bar_button = document.querySelector("#cookie-show-more");
+    var cookie_bar_button_close = document.querySelector("#cookie-close");
     var cookie_popup = document.querySelector("#cookie-popup");
     cookie_popup.style.display = "none";
     cookie_bar_button.onclick = this.show_more;
+    cookie_bar_button_close.onclick = this.close;
     window.localStorage.setItem("cookie_bar_appeared", true);
 
-    
+    document.getElementById('cookie-expand').style.display = "none";
+    document.getElementById('cookie-expand').onclick = this.expand;
 
     document.getElementById('consent-input').addEventListener('change',function(e){
         this.consent = e.target.checked;
+        setTimeout(function(){
+            cookie_popup.style.display = "none";
+        }.bind(this),500);
    }.bind(this),false);
     
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var path = require('path');
+
 module.exports = {
     module: {
       rules: [
@@ -8,7 +10,23 @@ module.exports = {
         {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
-        }
+        },
+        {
+          test: /\.(png|jpg|gif|svg)$/i,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 8192,
+              },
+            },
+          ],
+        },
       ],
     },
+    devServer: {
+      contentBase: path.join(__dirname, '.'),
+      compress: true,
+      port: 9000
+    }
   };
